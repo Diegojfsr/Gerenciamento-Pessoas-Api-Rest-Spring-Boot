63a179ebc0280133737cc9a2b5f8956706cc4b8b
Diego Jefferson <diegojfsr@gmail.com>
2021-09-01 09:38:35 -0300

    Sistema Gerenciamento Pessoas

---
+++ src/PhoneUtils.java
@@ -0,0 +1,21 @@
+public class PhoneUtils {
+
+    private static final String PHONE_NUMBER = "";
+    private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
+    private static final long PHONE_ID = 1L;
+
+    public static PhoneDTO createFakeDTO() {
+        return PhoneDTO.builder()
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+
+    public static Phone createFakeEntity() {
+        return Phone.builder()
+                .id(PHONE_ID)
+                .number(PHONE_NUMBER)
+                .type(PHONE_TYPE)
+                .build();
+    }
+}
